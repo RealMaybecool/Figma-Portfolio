74eebdd0b6c7eaeb16b19ffb355d195f3a13c883
RealMaybecool <atapioca30@pcstudents.us>
2026-03-05 22:18:53 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["tr.rbxcdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.rbxcdn.com",
+      },
+    ],
   },
 };
 
